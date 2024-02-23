e56f229ce3123bd4f747fddc617423b36d815fb1
Fiorella <fioreponce09@gmail.com>
2024-02-23 12:21:49 -0300

    commit3

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const ilust = document.getElementById('ilust');
 const nombre = document.getElementById('nombre');
+const contac = document.getElementById('conta');
+const proyec = document.getElementById('proye');
+const sobMi = document.getElementById('sobremi');
 
 nombre.addEventListener('mouseover', () => {
     ilust.src = "./imag/yoAnimIzq.png"; 
@@ -13,4 +16,31 @@ ilust.addEventListener('mouseover', () => {
 });
 ilust.addEventListener('mouseleave', () => {
     ilust.src = "./imag/yoAnimFren.png"; 
+});
+
+contac.addEventListener('mouseover', () => {
+    ilust.src = "./imag/yoAnimContacto.png"; 
+});
+contac.addEventListener('mouseleave', () => {
+    ilust.src = "./imag/yoAnimFren.png"; 
+});
+
+proyec.addEventListener('mouseover', () => {
+    ilust.src = "./imag/yoAnimProyec.png"; 
+});
+proyec.addEventListener('mouseleave', () => {
+    ilust.src = "./imag/yoAnimFren.png"; 
+});
+
+sobMi.addEventListener('mouseover', () => {
+    ilust.src = "./imag/yoAnimSobreMi.png"; 
+});
+sobMi.addEventListener('mouseleave', () => {
+    ilust.src = "./imag/yoAnimFren.png"; 
+});
+
+
+
+document.getElementById('hamburger-btn').addEventListener('click', function() {
+    document.getElementById('menu-hamburguesa').classList.toggle('active');
 });
